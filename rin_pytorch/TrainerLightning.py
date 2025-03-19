053f911d629744d4a8562f6e2a9a7e5e73355bef
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2025-03-19 09:36:13 +0100

    Update SLURM job configuration and training parameters
    - Increase the number of nodes from 1 to 4 in launch_lrz.sbatch
    - Change training configuration to use '128.yaml' instead of 'cifar.yaml'
    - Adjust training parameters in train.py to set num_nodes to 1 and devices to 1
    - Modify token mask ratio to 0 in TrainerLightning.py for updated masking logic
    - Update image resizing logic in ImageNetDataModule to use a patch size of 128

--- rin_pytorch/TrainerLightning.py
+++ rin_pytorch/TrainerLightning.py
@@ -167,17 +167,17 @@ def pad_to_max_size(batch, patch_size, tape_dim, transform=None):
     image_masks = torch.stack(image_masks).bool()
     pos_embs = torch.stack(pos_embs)
 
-    _, token_mask = create_random_token_mask(padded_images, mask_ratio=0.7)
+    _, token_mask = create_random_token_mask(padded_images, mask_ratio=0)
     
     visible_padded_images = []
     visible_patch_masks = []
     visible_pos_embs = []
     for i, (padded_image, patch_mask, pos_emb) in enumerate(zip(padded_images, patch_masks, pos_embs)):
-        # print(padded_image.shape, patch_mask.shape, pos_emb.shape)
-        # print(token_mask.shape)
-        # print(token_mask[i].shape)
-        # print(~token_mask[i].sum())
-        # print(token_mask[i].sum())
+        print(padded_image.shape, patch_mask.shape, pos_emb.shape)
+        print(token_mask.shape)
+        print(token_mask[i].shape)
+        print(~token_mask[i].sum())
+        print(token_mask[i].sum())
         # print(token_mask[i])
         visible_idx = ~token_mask[i]
         visible_padded_images.append(padded_image[visible_idx])
@@ -202,7 +202,7 @@ class ImageNetDataModule(LightningDataModule):
             ])
         else:
             self.transform = transforms.Compose([
-                transforms.Resize((128, 128)) if self.config["run"]["vanilla"] else ResizeMaxSide(64),
+                transforms.Resize((128, 128)) if self.config["run"]["vanilla"] else ResizeMaxSide(128),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
             ])
