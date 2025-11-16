72299cfe7ea7ac05196cefcd7bd0d238c496ea98
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2025-11-16 04:53:49 +0100

    Increase training batch size to 128 and implement gradient accumulation in TrainerLightning; adjust loss calculation and optimizer steps accordingly.

--- rin_pytorch/TrainerLightning.py
+++ rin_pytorch/TrainerLightning.py
@@ -55,9 +55,11 @@ class RinLightningModule(LightningModule):
         self.ema_update_every = trainer_config["ema_update_every"]
         self.clip_grad_norm = trainer_config.get("clip_grad_norm")
         self.train_batch_size = trainer_config["train_batch_size"]
+        self.grad_accum_steps = max(1, trainer_config.get("gradient_accumulation_steps", 1))
         self.log_images = trainer_config.get("log_to_wandb", True)
 
         self._should_profile_first_step = True
+        self._grad_accum_counter = 0
 
     def forward(self, batch_img, batch_class):
         return self.diffusion_model(batch_img, batch_class)
@@ -69,10 +71,12 @@ class RinLightningModule(LightningModule):
         batch_img, batch_class = batch
         batch_class = F.one_hot(batch_class, num_classes=self.num_classes).float()
 
-        opt.zero_grad(set_to_none=True)
+        if self._grad_accum_counter == 0:
+            opt.zero_grad(set_to_none=True)
 
         profiler_ctx = nullcontext()
         profiling_active = False
+        prof = None
         if self._should_profile_first_step:
             activities = [ProfilerActivity.CPU]
             if torch.cuda.is_available():
@@ -88,14 +92,27 @@ class RinLightningModule(LightningModule):
 
         with profiler_ctx as prof:
             loss = self.forward(batch_img, batch_class)
-            self.manual_backward(loss)
+            loss_to_backward = loss / self.grad_accum_steps
+            self.manual_backward(loss_to_backward)
 
-        if self.clip_grad_norm is not None:
-            torch.nn.utils.clip_grad_norm_(self.diffusion_model.parameters(), self.clip_grad_norm)
+        self._grad_accum_counter += 1
 
-        opt.step()
-        if scheduler is not None:
-            scheduler.step()
+        total_batches = getattr(self.trainer, "num_training_batches", None)
+        is_last_batch = (
+            isinstance(total_batches, int) and total_batches > 0 and (batch_idx + 1) == total_batches
+        )
+        should_step = self._grad_accum_counter >= self.grad_accum_steps or is_last_batch
+
+        if should_step:
+            if self.clip_grad_norm is not None:
+                torch.nn.utils.clip_grad_norm_(self.diffusion_model.parameters(), self.clip_grad_norm)
+
+            opt.step()
+            if scheduler is not None:
+                scheduler.step()
+
+            opt.zero_grad(set_to_none=True)
+            self._grad_accum_counter = 0
 
         self.log(
             "loss",
@@ -107,16 +124,21 @@ class RinLightningModule(LightningModule):
             batch_size=batch_img.size(0),
         )
 
-        current_lr = scheduler.get_last_lr()[0] if scheduler is not None else opt.param_groups[0]["lr"]
-        self.log(
-            "lr",
-            current_lr,
-            on_step=True,
-            prog_bar=False,
-            logger=True,
-            sync_dist=True,
-            batch_size=batch_img.size(0),
-        )
+        if should_step:
+            if scheduler is not None:
+                current_lr = scheduler.get_last_lr()[0]
+            else:
+                current_lr = opt.param_groups[0]["lr"]
+
+            self.log(
+                "lr",
+                current_lr,
+                on_step=True,
+                prog_bar=False,
+                logger=True,
+                sync_dist=True,
+                batch_size=batch_img.size(0),
+            )
 
         if profiling_active and prof is not None:
             total_flops = sum(event.flops for event in prof.key_averages() if event.flops)
@@ -142,12 +164,13 @@ class RinLightningModule(LightningModule):
                 self.print("Profiler executed but no FLOPs information was collected.")
             self._should_profile_first_step = False
 
-        current_step = self.global_step + 1
-        if current_step % self.ema_update_every == 0:
-            self._update_ema()
+        if should_step:
+            current_step = self.global_step + 1
+            if current_step % self.ema_update_every == 0:
+                self._update_ema()
 
-        if self.log_images and current_step % self.sample_every == 0:
-            self._log_samples(current_step)
+            if self.log_images and current_step % self.sample_every == 0:
+                self._log_samples(current_step)
 
         return loss
 
