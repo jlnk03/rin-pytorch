e4e1952c51a7da91bef7c4eda446a7cabd7a51c7
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2025-11-16 14:07:24 +0100

    Update training configuration to increase batch size to 192 and refactor gradient accumulation logic in RinLightningModule for improved clarity and performance.

--- rin_pytorch/TrainerLightning.py
+++ rin_pytorch/TrainerLightning.py
@@ -74,8 +74,8 @@ class RinLightningModule(LightningModule):
         self._trainer_step += 1
         should_log_samples = self.log_images and (self._trainer_step % self.sample_every == 0)
 
-        if self._grad_accum_counter == 0:
-            opt.zero_grad(set_to_none=True)
+        # if self._grad_accum_counter == 0:
+        opt.zero_grad(set_to_none=True)
 
         profiler_ctx = nullcontext()
         profiling_active = False
@@ -95,27 +95,26 @@ class RinLightningModule(LightningModule):
 
         with profiler_ctx as prof:
             loss = self.forward(batch_img, batch_class)
-            loss_to_backward = loss / self.grad_accum_steps
-            self.manual_backward(loss_to_backward)
+            self.manual_backward(loss)
 
-        self._grad_accum_counter += 1
+        # self._grad_accum_counter += 1
 
         total_batches = getattr(self.trainer, "num_training_batches", None)
         is_last_batch = (
             isinstance(total_batches, int) and total_batches > 0 and (batch_idx + 1) == total_batches
         )
-        should_step = self._grad_accum_counter >= self.grad_accum_steps or is_last_batch
+        # should_step = self._grad_accum_counter >= self.grad_accum_steps or is_last_batch
 
-        if should_step:
-            if self.clip_grad_norm is not None:
-                torch.nn.utils.clip_grad_norm_(self.diffusion_model.parameters(), self.clip_grad_norm)
+        # if should_step:
+        if self.clip_grad_norm is not None:
+            torch.nn.utils.clip_grad_norm_(self.diffusion_model.parameters(), self.clip_grad_norm)
 
-            opt.step()
-            if scheduler is not None:
-                scheduler.step()
+        opt.step()
+        if scheduler is not None:
+            scheduler.step()
 
-            opt.zero_grad(set_to_none=True)
-            self._grad_accum_counter = 0
+        opt.zero_grad(set_to_none=True)
+        self._grad_accum_counter = 0
 
         self.log(
             "loss",
@@ -127,21 +126,21 @@ class RinLightningModule(LightningModule):
             batch_size=batch_img.size(0),
         )
 
-        if should_step:
-            if scheduler is not None:
-                current_lr = scheduler.get_last_lr()[0]
-            else:
-                current_lr = opt.param_groups[0]["lr"]
+        # if should_step:
+        if scheduler is not None:
+            current_lr = scheduler.get_last_lr()[0]
+        else:
+            current_lr = opt.param_groups[0]["lr"]
 
-            self.log(
-                "lr",
-                current_lr,
-                on_step=True,
-                prog_bar=False,
-                logger=True,
-                sync_dist=True,
-                batch_size=batch_img.size(0),
-            )
+        self.log(
+            "lr",
+            current_lr,
+            on_step=True,
+            prog_bar=False,
+            logger=True,
+            sync_dist=True,
+            batch_size=batch_img.size(0),
+        )
 
         if profiling_active and prof is not None:
             total_flops = sum(event.flops for event in prof.key_averages() if event.flops)
@@ -167,10 +166,10 @@ class RinLightningModule(LightningModule):
                 self.print("Profiler executed but no FLOPs information was collected.")
             self._should_profile_first_step = False
 
-        if should_step:
-            current_step = self.global_step + 1
-            if current_step % self.ema_update_every == 0:
-                self._update_ema()
+        # if should_step:
+        current_step = self.global_step + 1
+        if current_step % self.ema_update_every == 0:
+            self._update_ema()
 
         if should_log_samples:
             self._log_samples(self._trainer_step)
